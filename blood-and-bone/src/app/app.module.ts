6cd3b17b2329a9efd2d5e6ff16bf1fcedc906af5
Nicholas Moore <myrmidon16@gmail.com>
2021-03-22 20:09:52 -0700

    Strawman management components.

--- blood-and-bone/src/app/app.module.ts
+++ blood-and-bone/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './characters/characters.component';
+import { BaseAttributesComponent } from './attributes/base-attributes/base-attributes.component';
+import { CombatAttributesComponent } from './attributes/combat-attributes/combat-attributes.component';
+import { SkillsComponent } from './attributes/skills/skills.component';
+import { WeaponsComponent } from './weapons/weapons.component';
+import { ArmorComponent } from './armor/armor.component';
+import { ImportComponent } from './admin/import/import.component';
+import { ExportComponent } from './admin/export/export.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharactersComponent
+    CharactersComponent,
+    BaseAttributesComponent,
+    CombatAttributesComponent,
+    SkillsComponent,
+    WeaponsComponent,
+    ArmorComponent,
+    ImportComponent,
+    ExportComponent
   ],
   imports: [
     BrowserModule,
---
+++ blood-and-bone/src/app/attributes/combat-attributes/combat-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-combat-attributes',
+  templateUrl: './combat-attributes.component.html',
+  styleUrls: ['./combat-attributes.component.sass']
+})
+export class CombatAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
